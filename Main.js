29cf05351e76ebb0f8b404f156594cd20aac72f9
Safa Andac <sand94@github.com>
2019-08-05 13:12:17 +0300

    python integration completed

---
+++ Main.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const {app, BrowserWindow, ipcMain} = require('electron');
+const {PythonShell} = require('python-shell');
+const {
+  CATCH_ON_MAIN,
+  SEND_TO_RENDERER,
+} = require('./src/utils/constants');
+const pythonDir = './src/python';
+
+let win;
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800, height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  }) ;
+
+  // and load the index.html of the app.
+  // win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
+  win.loadURL('http://localhost:3000');
+
+}
+
+ipcMain.on(CATCH_ON_MAIN, (event, arg) => {
+  PythonShell.run(path.join(__dirname, pythonDir, 'nope.py'), null, (err, results) => {
+    if (err) throw err;
+    win.send(SEND_TO_RENDERER, results);
+  });
+});
+
+
+
+app.on('ready', createWindow);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import HomePage from './components/HomePage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const {
+  CATCH_ON_MAIN,
+  SEND_TO_RENDERER,
+} = require('./utils/constants');
+
+const { ipcRenderer } = window;
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleOnClick = this.handleOnClick.bind(this);
+    this.handleRenderer = this.handleRenderer.bind(this);
+
+    this.state = {
+      python: 'empty',
+    };
+  }
+
+  componentDidMount() {
+    ipcRenderer.on(SEND_TO_RENDERER, this.handleRenderer);
+  }
+
+  componentWillUnmount() {
+    ipcRenderer.removeListener(SEND_TO_RENDERER,  this.handleRenderer);
+  }
+
+  handleRenderer(event, data) {
+    this.setState({ python: data });
+  }
+
+  handleOnClick() {
+    ipcRenderer.send(CATCH_ON_MAIN, 'ping');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Hello!</p>
+          <p>DeepNeuroBoun</p>
+          <p>{this.state.python}</p>
+          <button onClick={this.handleOnClick}>Send!</button>
+        </header>
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 export default App;
